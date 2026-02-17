8998b15f5d62b04f87fb63f09d26522ab98e37ad
Balram Gupta <balramgupta20101@gmail.com>
2026-02-17 22:14:42 +0530

    update axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+  baseURL: 'https://task-management-3989.onrender.com/api/v1',
 });
 
 API.interceptors.request.use((req) => {
